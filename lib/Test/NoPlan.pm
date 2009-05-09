515d8c151b1b4eb9fbbd68351c7544a3c38bf548
Duncan Ferguson <duncan_j_ferguson@yahoo.co.uk>
2009-05-09 16:34:45 +0100

    Vanilla module created by Module::Starter

---
+++ lib/Test/NoPlan.pm
@@ -0,0 +1,107 @@
+package Test::NoPlan;
+
+use warnings;
+use strict;
+
+=head1 NAME
+
+Test::NoPlan - The great new Test::NoPlan!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Test::NoPlan;
+
+    my $foo = Test::NoPlan->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 FUNCTIONS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Duncan Ferguson, C<< <duncan_j_ferguson at yahoo.co.uk> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-test-noplan at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Test-NoPlan>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Test::NoPlan
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Test-NoPlan>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Test-NoPlan>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Test-NoPlan>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Test-NoPlan/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2009 Duncan Ferguson, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+
+=cut
+
+1; # End of Test::NoPlan
